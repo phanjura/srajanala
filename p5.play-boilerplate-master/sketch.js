2bf78ff4b13021352da8984b63028ae96f6bab69
phanjura <puja.hanjura@gmail.com>
2020-05-21 15:15:55 +0530

    Add files via upload

--- p5.play-boilerplate-master/sketch.js
+++ p5.play-boilerplate-master/sketch.js
@@ -1,26 +1,45 @@
+var car,wall;
+var speed,weight;
+
 function setup() {
-  createCanvas(800,400);
-  fixedRect = createSprite(200,200,30,50);
-  fixedRect.shapeColor = "green";
-  movingRect = createSprite(400, 200, 80, 30);
-  movingRect.shapeColor = "green";
+  createCanvas(1600,400);
+
+  car = createSprite(50,200,50,50);
+
+  wall = createSprite(1500,200,60,height/2);
+  wall.shapeColor = "#FF007F";
+
+
+  speed = random(55,90);
+  weight = random(400,1500);
+
+  car.velocityX = speed;
+
+ car.collide(wall);
+
+  
 }
 
 function draw() {
   background(255,255,255);
-  movingRect.y = World.mouseY;
-  movingRect.x = World.mouseX;
-  if(movingRect.x-fixedRect.x<movingRect.width/2+fixedRect.width/2 &&
-    fixedRect.x-movingRect.x<movingRect.width/2+fixedRect.width/2 && 
-    movingRect.y-fixedRect.y<movingRect.height/2+fixedRect.height/2 &&
-    fixedRect.y-movingRect.y<movingRect.height/2+fixedRect.height/2 ) {
-    fixedRect.shapeColor = "red";
-    movingRect.shapeColor = "red";
-} else{
-    fixedRect.shapeColor = "green";
-    movingRect.shapeColor = "green";
-}
+  
+  if(wall.x-car.x<(car.width+wall.width/2)){
+    car.velocityX = 0;
+    var deformation = 0.5*weight*speed*speed/22509;
+  }
+  if(deformation>180){
+    car.shapeColor = color(255,0,0);
+     }
+  
+  if(deformation<180 && deformation>100){
+    car.shapeColor = color(230,230,0);
+  }
 
-   
+  if(deformation<100){
+    car.shapeColor = color(0,255,0);
+  }
+
+
+
   drawSprites();
 }
